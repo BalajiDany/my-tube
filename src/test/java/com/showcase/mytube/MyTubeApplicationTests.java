0d153e16ad7094311684a608ccc6db13131a9b71
Balaji <d.balaji.mc@gmail.com>
2019-06-10 20:45:47 +0530

    Initial commit

---
+++ src/test/java/com/showcase/mytube/MyTubeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.showcase.mytube;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyTubeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
